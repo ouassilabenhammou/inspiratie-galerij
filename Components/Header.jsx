e3991ae6c6dc0556c3b30615cca90e3558cfaf7f
Ouassila00 <145725226+Ouassila00@users.noreply.github.com>
2025-06-23 21:15:59 +0200

    basisstructuur van header toegevoegd

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Link from "next/link";
+import next from "next";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <Link href="/galerij">Galerij</Link>
+      <Link href="/login">Login</Link>
+      <Link href="/vind-ik-leuk">Vind ik leuk</Link>
+    </div>
+  );
 }
