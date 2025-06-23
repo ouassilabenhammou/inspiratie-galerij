65ae8f6720aca04e1ef9d8601af7f7881f308f71
Ouassila00 <145725226+Ouassila00@users.noreply.github.com>
2025-06-24 00:18:35 +0200

    firebase geïnstalleerd en header aangepast

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,14 +1,11 @@
-import React from "react";
+// Components/Header.jsx
 import Link from "next/link";
-import next from "next";
 
 export default function Header() {
   return (
-    <div>
-      <Link href="/">Home</Link>
+    <header className="p-4 shadow-md flex gap-4">
       <Link href="/galerij">Galerij</Link>
-      <Link href="/login">Login</Link>
       <Link href="/vind-ik-leuk">Vind ik leuk</Link>
-    </div>
+    </header>
   );
 }
